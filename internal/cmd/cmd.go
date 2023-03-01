ab8e38fe0be7c4e160227a20a9dc02f729ab0a53
sbchild <a15315862177@163.com>
2023-03-02 03:33:32 +0800

    init framework

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -17,26 +17,45 @@ var (
 		Name:  "main",
 		Usage: "main",
 		Brief: "start http server",
-		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
-			s := g.Server()
-			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
-				group.Bind(
-					controller.Hello,
-				)
-			})
-			s.Start()
-			gproc.AddSigHandlerShutdown(func(sig os.Signal) {
-				g.Log().Infof(ctx, "%s Signal received, stopping service...", sig.String())
-				s.Shutdown()
-				// stop other services
-				// ---
-			})
-			gproc.Listen()
-			g.Wait()
-			// wait other services
-			// ---
-			return nil
-		},
+		Func:  startHttpServer,
 	}
 )
+
+func startHttpServer(ctx context.Context, parser *gcmd.Parser) (err error) {
+	// main server is used for frontend
+	mainServer := g.Server()
+	mainServer.Group("/", func(group *ghttp.RouterGroup) {
+		group.Middleware(ghttp.MiddlewareHandlerResponse)
+		group.Bind(
+			// todo
+			controller.Hello,
+		)
+	})
+	// proxy servers are handle HTTP requests, do some scanning,
+	// then forward to origin server or redirect to main server
+	// todo
+	// ---
+	// start the main server
+	mainServer.Start()
+	// start proxy servers
+	// todo
+	// ---
+	// if a shutdown signal received
+	gproc.AddSigHandlerShutdown(func(sig os.Signal) {
+		g.Log().Infof(ctx, "%s Signal received, stopping service...", sig.String())
+		// stop the main server
+		mainServer.Shutdown()
+		// stop proxy servers
+		// todo
+		// ---
+		// stop other services
+		// ---
+	})
+	// listen for signals
+	gproc.Listen()
+	// wait for http servers stopping
+	g.Wait()
+	// wait for other services stopping
+	// ---
+	return nil
+}
--- internal/controller/hello.go
+++ internal/controller/hello.go
@@ -3,8 +3,8 @@ package controller
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/frame/g"
 	v1 "seifu-gw/api/v1"
+	// "github.com/gogf/gf/v2/frame/g"
 )
 
 var (
@@ -14,6 +14,7 @@ var (
 type cHello struct{}
 
 func (c *cHello) Hello(ctx context.Context, req *v1.HelloReq) (res *v1.HelloRes, err error) {
-	g.RequestFromCtx(ctx).Response.Writeln("Hello World!")
+	// g.RequestFromCtx(ctx).Response.Writeln("Hello World!")
+	res = &v1.HelloRes{}
 	return
 }
